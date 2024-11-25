fb29ecd9502031e1024efb87076b4d2f8992fd9d
Muchach0 <guillaume.vilar@gmail.com>
2024-11-25 09:46:27 +0100

    Enemies - fix spawning enemy initial position

--- scripts/boss/EnemySpawner.gd
+++ scripts/boss/EnemySpawner.gd
@@ -1,6 +1,9 @@
 extends Enemy
 class_name EnemySpawner
 
+
+onready var spawn_points := $SpawnPoints if has_node("SpawnPoints") else null
+
 # Class to spawn enemies
 
 
@@ -8,13 +11,25 @@ class_name EnemySpawner
 func _ready():
 	pass
 
+func get_closer_spawn_point(position_player: Vector2) -> Vector2:
+	if spawn_points == null:
+		print_debug("EnemySpawner.gd - get_closer_spawn_point - no spawn points")
+		return position
+	var closest_spawn_point = Vector2()
+	var closest_distance = INF # Infinity - very large number for initialization
+	for spawn_point in spawn_points.get_children():
+		var distance = position_player.distance_to(spawn_point.global_position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_spawn_point = spawn_point.global_position
+	return closest_spawn_point
 
 remotesync func spawn_ennemy():
-	print_debug("EnemySpawner.gd - spawn_ennemy() - spanning an ennemy")
-	var enemy = preload("res://prefab/enemies/Spider_1.tscn").instance()
-	enemy.set_name("Spider_spawned_1")
-	enemy.position = position
-	get_node("..").add_child(enemy)
+	print_debug("EnemySpawner.gd - spawn_ennemy() - spawning an ennemy")
+	var enemy_to_spawn = preload("res://prefab/enemies/Spider_1.tscn").instance()
+	enemy_to_spawn.set_name("Spider_spawned_1")
+	enemy_to_spawn.global_position = get_closer_spawn_point(player.global_position)
+	get_node("..").add_child(enemy_to_spawn)
 
 
 #================ ATTACK PART ================
